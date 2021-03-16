4f3d01d39f67d825f07956ff4eaee7929cc20802
B-G Seo <bumku1234@gmail.com>
2021-03-16 19:42:44 +0900

    Practice branch: add onResume()

--- app/src/main/java/com/example/bbam1/MainActivity.java
+++ app/src/main/java/com/example/bbam1/MainActivity.java
@@ -3,17 +3,51 @@ package com.example.bbam1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String Tag = "서범규의 Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(Tag,"onCreate called!");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.e(Tag,"onResume called!");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(Tag,"onStart called!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(Tag,"onStop called!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(Tag,"onDestroy called!");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(Tag,"onPause called!");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(Tag,"onRestart called!");
     }
 }
